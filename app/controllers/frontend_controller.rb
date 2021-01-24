3b767e64f64acb2b3ad529e913759728529ec3e3
Arman Adhitama Prawirasasra <arman.a.prawirasasra@synergyengineering.com>
2021-01-24 12:36:44 +0700

    fix redirection logic

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -6,17 +6,23 @@ class FrontendController < ApplicationController
   protected
 
   def check_location
-    return if params[:country_user_country].present? && params[:user_city].present?
+    return if cookies[:country] == params[:country_user_country] && cookies[:city] == params[:user_city]
 
-    if cookies[:country] != params[:country_user_country] || cookies[:city] != params[:user_city]
+    if params[:country_user_country].present? && params[:user_city].present?
+      @country = Country.find_by(slug: params[:country_user_country])
+      @city = @country&.cities&.find_by(slug: params[:user_city])
+    else
       user_ip = Rails.env.development? ? '158.140.178.94' : request.ip
       geocoded = Geocoder.search(user_ip).first
-      country = Country.create_with(name: IsoCountryCodes.find(geocoded.country).name).create_or_find_by(slug: geocoded.country.downcase)
-      cookies[:country] = country.slug
-      city = country.cities.create_with(name: geocoded.city).create_or_find_by(slug: geocoded.city.parameterize)
-      cookies[:city] = city.slug
+      @country = Country.create_with(name: IsoCountryCodes.find(geocoded.country).name)
+                        .create_or_find_by(slug: geocoded.country.downcase)
+      @city = @country.cities.create_with(name: geocoded.city)
+                      .create_or_find_by(slug: geocoded.city.parameterize)
+
+      redirect_to "/#{@country.slug}/#{@city.slug}"
     end
 
-    redirect_to "/#{cookies[:country]}/#{cookies[:city]}"
+    cookies[:country] = @country.slug
+    cookies[:city] = @city.slug
   end
 end
